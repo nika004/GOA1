75d33dafb522cb49c43a095d84246a7e36205ef1
nika004 <janezashvilinikusha@gmail.com>
2026-02-07 21:21:34 +0400

    commit

---
+++ day48/homework/home.py
@@ -0,0 +1,108 @@
+# ვისაც დაგრჩათ 8kyu'იანები, გააკეთეთ 20+ 8kyu
+
+# Opposite number
+# def opposite(number):
+#     return number*(-1)
+
+# Find the position!
+# def position(letter):
+#     alphabet = ["2","a", "b", "c", "d", "e", "f", "g","h", "i","j", "k", "l", "m", "n", "o", "p", "q","r", "s", "t", "u","v", "w", "x", "y", "z"]
+#     indx= alphabet.index(letter)
+#     return f"Position of alphabet: {indx}"
+
+# Fizz Buzz
+# def fizzbuzz(n):
+#     l = []
+#     for i in range(1,n+1):
+#         if i % 3 == 0 and i % 5 == 0:
+#             l.append("FizzBuzz")
+#         elif i % 3 == 0:
+#             l.append("Fizz")
+#         elif i % 5 == 0:
+#             l.append("Buzz")
+#         else:
+#             l.append(i)
+#     return l
+
+
+# Invert values
+# def invert(lst):
+#     l=[]
+#     for i in lst:
+#         l.append(-i)
+#     return l
+
+# Convert a Number to a String!
+# def number_to_string(num):
+#      return str(num)
+
+# Return Negative
+# def make_negative( number ):
+#      return -abs(number)
+
+# Reversed Strings
+# def solution(string):
+#     return string[::-1]
+
+# Returning Strings
+# def greet(name):
+#     return f"Hello, {name} how are you doing today?"
+
+# Beginner - Lost Without a Map
+# def maps(a):
+#     return [x*2 for x in a]
+
+# Convert a string to an array
+# def string_to_array(s):
+#     return s.split(" ")
+
+# How good are you really?
+# def better_than_average(class_points, your_points):
+#     num=sum(class_points) / len(class_points)
+#     return your_points > num
+
+
+# def get_count(sentence):
+#     count = 0
+
+#     vowels = "aeiou"
+
+#     for i in sentence:
+#         if i in vowels:
+#             count += 1
+#     return count
+
+# def bin_to_decimal(inp):
+#      return int(inp, 2)
+
+# def close_compare(a, b, margin=0):
+#     if abs(a - b) <= margin:
+#         return 0
+#     elif a < b:
+#         return -1
+#     else:
+#         return 1
+
+# Sum of Multiples
+# def sum_mul(n, m):
+#     if n <= 0 or m <= 0:
+#         return "INVALID"
+    
+#     total = 0
+#     for i in range(n, m, n):
+#         total += i
+#     return total
+
+# Sentence Smash
+# def smash(words):
+#     return " ".join(words)
+    
+# Sentence Smash
+# Python:
+# def smash(words):
+#     return " ".join(words)
+    
+# Sentence Smash
+# def smash(words):
+#     return " ".join(words)
+    
