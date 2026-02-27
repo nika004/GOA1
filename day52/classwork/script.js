ea497e3e42f3890ea2e1ddf5e3246c0a07523981
nika004 <janezashvilinikusha@gmail.com>
2026-02-27 17:32:35 +0400

    commit

---
+++ day52/classwork/script.js
@@ -0,0 +1,51 @@
+// let ცვლადი შეგვიძლია შევცვალოთ
+// const ცვლადი კი არ შეგვიძლია შევცვალოთ
+
+
+let name1 ="nika";
+let name2 ="saba";
+let name3 ="gio";
+let name4 ="mari";
+let name5 ="salo";
+
+
+let num1 ="16";
+let num2 ="1"
+let num3="62";
+let num4 ="0"
+let num5 ="32";
+
+
+
+console.log(name)
+console.log(name2)
+console.log(name3)
+console.log(name4)
+console.log(name5)
+
+console.log(age1)
+console.log(age2)
+console.log(age3)
+console.log(age4)
+console.log(age5)
+
+
+console.log(5 + 6)
+console.log(2 * 2)
+console.log(2 / 8)
+console.log(3 % 3)
+console.log(11 + 4)
+
+
+
+let num6 =16;
+let num7 =11;
+let num8 =62;
+let num9 =30;
+let num10 =32;
+
+console.log(String(age6))
+console.log(String(age2))
+console.log(String(age3))
+console.log(String(age4))
+console.log(String(age5))
---
+++ day52/homework/script.js
@@ -0,0 +1,45 @@
+// javascript-თ შეგვიძლია საიტი გავხადოთ ფუნქციონალური
+// scripting language არის ენა რომელიც წერს სკრიპტს 
+// number , string ,boolean, null, undefined და object
+
+console.log(5*5);
+console.log(5/5);
+console.log(5-5);
+console.log(7+1);
+console.log(6%2);
+
+console.log(5/5);
+console.log(2*6);
+console.log(5*56);
+console.log(5+5);
+console.log(9-5);
+
+let str1 =("nika");
+console.log(typeof str1)
+
+let num1 =(6);
+console.log(typeof num1)
+
+let str2 =("ahue a");
+console.log(typeof str2)
+
+let bool =(5=5);
+console.log(typeof bool)
+
+let num2 =(44);
+console.log(typeof num2)
+
+let nl =(null);
+console.log(typeof nl)
+
+// let un =( );
+// console.log(typeof un)
+
+let num3 =(334);
+console.log(typeof num3)
+
+let str4 =("dadwa");
+console.log(typeof str4)
+
+let str5 =("adway");
+console.log(typeof str5)
